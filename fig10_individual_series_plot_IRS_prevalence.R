76909a838308661370f94b3acf908562a6165d67
MelanieLoeffel <101869595+MelanieLoeffel@users.noreply.github.com>
2022-06-09 15:38:46 +0200

    Update fig10_individual_series_plot_IRS_prevalence.R

--- fig10_individual_series_plot_IRS_prevalence.R
+++ fig10_individual_series_plot_IRS_prevalence.R
@@ -4,6 +4,7 @@
 
 
 source("preparation_analysis.R")
+library(stringr)
 
 
 #adding row numbers
@@ -12,6 +13,8 @@ data_new_prev$row_number<-seq.int(nrow(data_new_prev))
 first_row_study<-data_new_prev[!duplicated(data_new_prev$study_number_new),]
 first_row_study$row_number<-seq.int(nrow(first_row_study))
 first_row_study$study_area_add<-gsub(".*,","", x=first_row_study$study_area)
+first_row_study$study_area_add<-str_trim(first_row_study$study_area_add)
+first_row_study$study_area_add<-str_replace(first_row_study$study_area_add, "^\\w{1}", toupper)
 first_row_study$study_area<-gsub(",.*","", x=first_row_study$study_area)
 first_row_study$first_authors<-gsub(",.*","", x=first_row_study$first_authors)
 first_row_study$study_area[first_row_study$study_area=='Papua New Guinea']<-'PNG'
@@ -163,6 +166,8 @@ for (val in x)
   text(45,0.8,fave_cols$letter[fave_cols$number==val],cex=1.2)
   abline(h = 0, col = "gray80")}
 
+plot(pr_numb_vivax_LM/(pr_numb_falciparum_LM+pr_numb_vivax_LM)~time_zero,type='n', data=data_new_prev, bty='n', xaxt='n', yaxt='n', ylab='', xlab='')
+legend('topleft',legend=c('<100 cases', '100-199 cases', '200-499 cases', '500-999 cases', '>999 cases'), col=c('navy'), pch=c(16), pt.cex=c(2*0.75, 2*1, 2*1.25, 2*1.5, 2*1.75), cex=1.5)
 
 
 
